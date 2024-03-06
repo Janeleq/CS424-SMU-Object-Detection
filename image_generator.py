5553ff88250fc0790e701c0bc9af9674a225d11c
Janeleq <janelim2001@gmail.com>
2024-03-06 11:23:00 +0800

    Update dataset and folder/file paths

--- image_generator.py
+++ image_generator.py
@@ -1,13 +1,25 @@
 import requests
 from PIL import Image
+import os.path
+import cv2 
 
-def img_requests(txt):
-    response=requests.get("https://source.unsplash.com/random{0}".format(txt))
-    file=open('image.jpg','wb')
-    file.write(response.content)
-    img=Image.open(r"image.jpg")
-    img.show()
-    file.close()
+save_path = 'C:/wamp64/www/CS424-G1/data/image_classification_smu/validation/non-smu'
+
+def img_requests(txt, save_folder):
+     response=requests.get("https://source.unsplash.com/random{0}".format(txt))
+    # Ensure the save folder exists, create it if not
+     if not os.path.exists(save_folder):
+          print(f'Creating folder {save_folder}')
+          os.makedirs(save_folder)
+
+     img_path = os.path.join(save_folder, f'image_{i}.jpg')  # Save images with a unique name
+     # with open(img_path, 'wb') as file:
+     #      file.write(response.content)
+     #      print("writing file")
+          
+     img = Image.open(img_path)
+
+     # return img_path
 
 print("""Please provide an option for Image
      1. HD Random Picture
@@ -18,7 +30,9 @@ print("""Please provide an option for Image
 
 ans=input()
 image_list = []
+# completeName = os.path.join(save_path, name_of_file+".txt")         
 
+# file1 = open(completeName, "w")
 if 'one' in ans or '1' in ans:
      print("Please wait while we fetch the images from our database.")
      for i in range(100):
@@ -45,8 +59,10 @@ elif 'five' in ans or '5' in ans:
      st=input()
      st="?"+st
      print("Please wait while we fetch the images from our database.")
-     for i in range(100):
-        img_requests(st)
+     for i in range(500):
+        img = img_requests(st, save_path)
+        image_list.append(img)
+
 else:
      print("Please provide a valid input.")
 
